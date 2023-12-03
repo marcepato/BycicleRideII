ed9ca64d7eb248847627bb01ad8ea596828e8302
marcehiguera <marcehiguera@gmail.com>
2023-12-03 19:39:32 -0300

    subir el proyecto

---
+++ js/transporte_editar.js
@@ -0,0 +1,77 @@
+console.log(location.search)     // lee los argumentos pasados a este formulario
+var id=location.search.substr(4)  // producto_update.html?id=1
+console.log(id)
+
+const { createApp } = Vue
+  createApp({
+    data() {
+      return {
+        id:0,
+        descripcion:"",
+        rodado:"",
+        tipo:"",
+        grupoEtario:"",
+        genero:"",
+        suspension:"",
+        frenos:"",
+        velocidad:"",
+        foto:"",
+       // url:'http://marcerda.pythonanywhere.com/productos/'+id,
+        url: 'http://127.0.0.1:5000/bycicles/'+id,
+       }  
+    },
+    methods: {
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.id=data.id;
+                    this.descripcion = data.descripcion;
+                    this.rodado=data.rodado;
+                    this.tipo=data.tipo;
+                    this.grupoEtario=data. grupoEtario    ;    
+                    this.genero=data.genero;
+                    this.suspension = data.suspension;
+                    this.frenos=data.frenos;
+                    this.velocidad=data.velocidad;
+                    this.foto=data.foto               
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.error=true              
+                })
+        },
+        modificar() {
+            let transporte = {
+                descripcion:this.descripcion,
+                rodado: this.rodado,
+                tipo: this.tipo,
+                grupoEtario: this.grupoEtario,
+                genero: this.genero,
+                suspension: this.suspension,
+                frenos: this.frenos,
+                velocidad: this.velocidad,
+                foto: this.foto 
+            }
+            var options = {
+                body: JSON.stringify(transporte),
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro modificado")
+                    window.location.href = "./transporte.html"; // navega a productos.html          
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Modificar")
+                })      
+        }
+    },
+    created() {
+        this.fetchData(this.url)
+    },
+  }).mount('#app')
---
+++ js/transportes.js
@@ -0,0 +1,85 @@
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            transportes: [],
+          //  url:    "https://marcerda.pythonanywhere.com/productos",  //
+           url: "http://127.0.0.1:5000/bycicles",
+      
+            descripcion: " ",
+            rodado: " ",
+            tipo:  " ",
+            grupoEtario: "",
+            genero: " ",
+            suspension:  " ",
+            frenos:  " ",
+            velocidad: "",
+            foto: "",
+            error:false,
+            cargando:true
+        }
+    },
+    methods: { // aqui se definen  las funciones del objeto VUE
+
+        fetchData(url) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data)
+                    this.transportes = data
+                    this.cargando=false
+                })
+                .catch(error => {//alert("Ups... se produjo un error: " + error)
+                this.error=true
+            })
+        },
+
+        grabar() {
+            let transporte = {
+                descripcion: this.descripcion,
+                rodado: this. rodado,
+                tipo: this.tipo,
+                grupoEtario: this.grupoEtario,
+                genero: this.genero,
+                suspension: this.suspension,
+                frenos: this.frenos,
+                velocidad: this.velocidad,
+                foto: this.foto,
+            }
+            var options = {
+                body: JSON.stringify(transporte),
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                redirect: 'follow'
+            }
+            fetch(this.url, options)
+                .then(function () {
+                    alert("Registro grabado")
+                    window.location.href = "./transporte.html";  // recarga productos.html
+                })
+                .catch(err => {
+                    console.error(err);
+                    alert("Error al Grabar")  // puedo mostrar el error tambien
+                    this.error=true
+                })
+        },
+        eliminar(id) {
+            const url = this.url + '/' + id;
+            var options = {
+                method: 'DELETE',
+            }
+            fetch(url, options)
+                .then(res => res.text()) // or res.json()
+                .then(res => {
+                    alert('Registro Eliminado')
+                    location.reload(); // recarga el json luego de eliminado el registro
+                })
+        },
+
+
+    },
+
+    created() {    //Se ejecuta cuando se carga el objeto VUE
+        this.fetchData(this.url)
+    }
+}).mount('#app')
